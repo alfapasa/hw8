f6371f4676e7bd8e19e584c4a0156dc01b4820a3
liza <elizavetakulik2@gmail.com>
2024-02-12 13:11:03 +0100

    commit

--- js/hw8_3.js
+++ js/hw8_3.js
@@ -1,10 +1,10 @@
-// // Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+// Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
 
-// let n = parseInt(prompt('Write the number'));
+let n = parseInt(prompt('Write the number'));
 
-// for (let a = 1; a <= 100; a++) {
-//     if ((a * a) >= n) {
-//         break;
-//     }
-//     console.log(a);
-// }
+for (let a = 1; a <= 100; a++) {
+    if ((a * a) >= n) {
+        break;
+    }
+    console.log(a);
+}
